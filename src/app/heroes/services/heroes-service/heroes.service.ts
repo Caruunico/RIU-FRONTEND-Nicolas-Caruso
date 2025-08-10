7957aa856e4a39f2bc52aee8c0f5d893973785af
caruunico <nicolascaruso19@outlook.com>
2025-08-09 23:10:37 -0300

    last commit continuation

--- src/app/heroes/services/heroes-service/heroes.service.ts
+++ src/app/heroes/services/heroes-service/heroes.service.ts
@@ -7,25 +7,11 @@ import { Hero } from '../../interfaces/heroe.interface';
 export class HeroesService {
   public heroes = signal<Hero[]>([]);
   public selectedHero = signal<Hero | null>(null);
-  public filterTerm = signal('');
-  public filteredHeroes = computed(() => {
-    const term = this.filterTerm().trim().toLowerCase();
-    if (!term) return this.heroes();
-    return this.heroes().filter(hero =>
-      hero.name.toLowerCase().includes(term)
-    );
-  });
 
   constructor() { }
 
   public setHeroes(heroes: Hero[]) {
     this.heroes.set(heroes);
   }
-
-  public setFilterTerm(term: string) {
-    this.filterTerm.set(term);
-  }
-
-
 
 }
