569b2d39f68c2f62035519c79bc320dfc7de13ff
caruunico <nicolascaruso19@outlook.com>
2025-08-09 23:10:25 -0300

    feat: -set bootstrap in angular.json -creation service hero for set heroes -creation headerComponent -event table filter -paginator fix when filtering -config tsconfig.app.json

--- src/app/heroes/services/heroes-service/heroes.service.ts
+++ src/app/heroes/services/heroes-service/heroes.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { Hero } from '../../interfaces/heroe.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+  public heroes = signal<Hero[]>([]);
+  public selectedHero = signal<Hero | null>(null);
+  public filterTerm = signal('');
+  public filteredHeroes = computed(() => {
+    const term = this.filterTerm().trim().toLowerCase();
+    if (!term) return this.heroes();
+    return this.heroes().filter(hero =>
+      hero.name.toLowerCase().includes(term)
+    );
+  });
 
   constructor() { }
+
+  public setHeroes(heroes: Hero[]) {
+    this.heroes.set(heroes);
+  }
+
+  public setFilterTerm(term: string) {
+    this.filterTerm.set(term);
+  }
+
+
+
 }
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil, distinctUntilChanged } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, MatInputModule, ReactiveFormsModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Input() title: string = '';
+  @Input() totalRegistros?: number = 0;
+  @Input() icon?: string = '';
+
+  @Output() searchEvent = new EventEmitter<string>();
+
+  public search: FormControl = new FormControl('');
+
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    console.log(this.totalRegistros)
+    this.search.valueChanges
+      .pipe(
+        distinctUntilChanged((prev, next) => JSON.stringify(prev) === JSON.stringify(next)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((filterTerm: string) => {
+        this.searchEvent.emit(filterTerm);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
